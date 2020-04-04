164b1b7ef9d75084f44e55ed74714a4967aaafd6
517030910405 <lijiasen0921@126.com>
2020-04-04 23:36:43 +0800

    update

---
+++ acmdb-lab1/src/java/simpledb/so.java
@@ -0,0 +1,10 @@
+package simpledb;
+
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.TreeMap;
+public class so {
+    Map<Integer,Integer> map = new TreeMap<>();
+    Map<Integer,Integer> map2 = new HashMap<>();
+}
